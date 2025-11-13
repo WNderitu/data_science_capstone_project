47889c0a83be980b34df7afc826ef500a3e805f7
WNderitu <hildah.nderituh@gmail.com>
2025-11-13 18:39:33 +0300

    Update app.py

--- deployment/app.py
+++ deployment/app.py
@@ -176,12 +176,12 @@ if uploaded_files and net and class_names:
                 
                 st.metric("**Total Parasite Count (All Stages)**", total_parasite_count)
                 st.metric(
-                    "label='**Estimated Parasitemia Rate**",
+                    label='**Estimated Parasitemia Rate**',
                     value=parasitemia_display,
                     help=(
-                        "Calculated as:\n\n"
-                        "(Total Parasite Count ÷ Total Detections) × 100\n\n".
-                        "It estimates the proportion of infected cells among all detected cells."
+                        'Calculated as:\n\n'
+                        '(Total Parasite Count ÷ Total Detections) × 100\n\n'.
+                        'It estimates the proportion of infected cells among all detected cells'.
                         )
                     )
                 st.info(f"**Total Objects Counted:** {total_detections}")
